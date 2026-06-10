61fa0da15769dbeeaf70137e9b32582455df8838
abd-ulbasit <basit01035@gmail.com>
2026-06-10 20:00:44 +0500

    feat: scaffold upgradescope module, root command, CI

---
+++ internal/cli/root.go
@@ -0,0 +1,16 @@
+package cli
+
+import "github.com/spf13/cobra"
+
+var version = "dev" // set via -ldflags
+
+func Root() *cobra.Command {
+	root := &cobra.Command{
+		Use:           "upgradescope",
+		Short:         "Continuous Kubernetes upgrade-readiness scanner",
+		Version:       version,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	return root
+}
